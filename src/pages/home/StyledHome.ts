1be5753e801cca642a7c65d7dbc589fe442d257b
Sam <samvelgrigoryanteded@gmail.com>
2024-03-28 12:17:33 +0400

    mobile style fixes

--- src/pages/home/StyledHome.ts
+++ src/pages/home/StyledHome.ts
@@ -14,20 +14,37 @@ export const StyledPreferencesWrapper = styled.div`
     display: flex;
     align-items: center;
     margin: 0 -10px 20px;
+    
+    @media (max-width: 768px) {
+        flex-direction: column;
+    }
 `;
 
 export const StyledPreferenceWrapper = styled.div`
     flex: 1;
     padding: 0 10px;
+    
+    @media (max-width: 768px) {
+        margin-bottom: 10px;
+        width: 100%;
+    }
 `;
 
 export const StyledSelectionWrapper = styled.div`
     display: flex;
     align-items: center;
     padding: 10px 0 20px;
+    
+    @media (max-width: 768px) {
+        flex-direction: column;
+    }
 `;
 export const StyledLabel = styled.label`
     margin-right: 20px;
+    
+    @media (max-width: 768px) {
+        margin-bottom: 10px;
+    }
 `;
 export const StyledCheckBox = styled.input`
     margin-right: 5px;
